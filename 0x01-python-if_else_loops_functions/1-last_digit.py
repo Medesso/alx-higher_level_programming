a343da2da1a2f7c6e73a95fde8c187c836ea9496
root <essouani.med@gmail.com>
2023-10-31 16:50:56 -0700

    mod

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,36 @@
-#!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
-digit = abs(number) % 10
-if number < 0:
-    digit = -digit
-print(f"Last digit of {number:d} is {digit:d} and is ", end="")
-if digit > 5:
-    print("greater than 5")
-elif digit == 0:
-    print("0")
-else:
-    print("less than 6 and not 0")
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Conditional to check if a number is >5, 0 or < 6
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, num;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	num = n % 10;
+	if (num > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, num);
+	}
+	else if (num == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, num);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, num);
+	}
+	return (0);
+}
+
+
 
