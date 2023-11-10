86fb913e67195e36939c9c1c1b693a6e6eddbfd4
root <essouani_med@gmail.com>
2023-11-09 16:31:49 -0800

    aed

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python3
+
+
 def complex_delete(a_dictionary, value):
-    list_keys = list(a_dictionary.keys())
+    new_dict = dict(a_dictionary)
+    for k, v in new_dict.items():
+        if v == value:
+            a_dictionary.pop(k)
+    return a_dictionary
 
-    for value_dic in list_keys:
-        if value == a_dictionary.get(value_dic):
-            del a_dictionary[value_dic]
 
-    return (a_dictionary)
+if __name__ == '__main__':
+    print_sorted_dictionary = \
+        __import__('6-print_sorted_dictionary').print_sorted_dictionary
+
+    a_dictionary = {'lang': "C", 'track': "Low", 'pref': "C", 'ids': [1, 2, 3]}
+    new_dict = complex_delete(a_dictionary, 'C')
+    print_sorted_dictionary(a_dictionary)
+    print("--")
+    print_sorted_dictionary(new_dict)
+
+    print("--")
+    print("--")
+    new_dict = complex_delete(a_dictionary, 'c_is_fun')
+    print_sorted_dictionary(a_dictionary)
+    print("--")
+    print_sorted_dictionary(new_dict)
 
